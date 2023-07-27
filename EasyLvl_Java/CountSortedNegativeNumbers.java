f21cc8926b91b6117e57d3eff3ab8d571a8be4ba
dbrown2251 <137955720+dbrown2251@users.noreply.github.com>
2023-07-26 19:38:06 -0500

    Easy Level - Leetcode (Java)

---
+++ EasyLvl_Java/CountSortedNegativeNumbers.java
@@ -0,0 +1,15 @@
+package EasyLvl_Java;
+
+public class CountSortedNegativeNumbers {
+    public int countNegatives(int[][] grid) {
+        int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] < 0) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
---
+++ EasyLvl_Java/FinalValueAfterPerformingOperations.java
@@ -0,0 +1,16 @@
+package EasyLvl_Java;
+
+public class FinalValueAfterPerformingOperations {
+    public int finalValueAfterOperations(String[] operations) {
+        int xValue = 0;
+        for (String operation : operations) {
+            if (operation.contains("--")) {
+                xValue--;
+            }
+            else if (operation.contains("++")) {
+                xValue++;
+            }
+        }
+        return xValue;
+    }
+}
