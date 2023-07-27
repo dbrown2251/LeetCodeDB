4ace8aa4027861f57c277caed01596f556959c1a
dbrown2251 <137955720+dbrown2251@users.noreply.github.com>
2023-07-26 19:57:40 -0500

    GroupAnagrams.java for medium level problem on LeetCode.com

---
+++ MediumLvl_Java/GroupAnagrams.java
@@ -0,0 +1,24 @@
+package MediumLvl_Java;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GroupAnagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] charArray = str.toCharArray();
+            Arrays.sort(charArray); // sorts charArray in ABC order
+            String sortedStr = new String(charArray);
+
+            if (!map.containsKey(sortedStr)) {
+                map.put(sortedStr, new ArrayList<>());
+            }
+            map.get(sortedStr).add(str);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
