570bbb367f07b9ec2111aab73da54fa8891a6645
dbrown2251 <137955720+dbrown2251@users.noreply.github.com>
2023-09-03 20:15:07 -0500

    Update GroupAnagrams.java
    got rid of package and imports

--- MediumLvl_Java/GroupAnagrams.java
+++ MediumLvl_Java/GroupAnagrams.java
@@ -1,11 +1,3 @@
-package MediumLvl_Java;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public class GroupAnagrams {
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> map = new HashMap<>();
